a675401193ac08bc2a59e385b7bd4c935e367f5d
Stanisław Grochowski <grochowski0stanislaw@gmail.com>
2025-04-09 15:39:28 +0200

    added cors

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from audiogram_parser import *
 from flask import Flask, request, jsonify
-
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/', methods=['GET'])
 def home():
