b8fd7b6c08f23a9fae0287c4cec0c0427f66960b
MarinaGalanina <marina.galanina.2000@mail.ru>
2025-05-06 09:36:37 +0200

    Comments add

--- audiogram_parser_1.py
+++ audiogram_parser_1.py
@@ -1,20 +1,34 @@
 import cv2
 import pandas as pd
+import numpy as np
+from data_normalization import *
 
 
+def drop_bone_data(df):
     """
+    Removes lower threshold duplicates (we assume bone result is better than air)
     """
+    duplicates = df[df.duplicated(subset=['Frequency (Hz)', 'Ear'], keep=False)]
+    air_data = duplicates.loc[duplicates.groupby(['Frequency (Hz)', 'Ear'])['Threshold (dB HL)'].idxmax()]
+    bone_data = duplicates.drop(air_data.index)
+    return df.drop(bone_data.index)
 
 
 def find_points(mask: np.ndarray, image: np.ndarray, ear_label: str, color: tuple) -> list:
     """
+    Detects audiogram points using contour detection.
 
+    Returns a list of pixel coordinates.
     """
     points = []
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for cnt in contours:
         x, y, w, h = cv2.boundingRect(cnt)
+        if 3 < w and 3 < h:
+            center_x = x + w / 2
+            center_y = y + h / 2
+            points.append([center_x, center_y, ear_label])
             cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
 
     return points
@@ -36,6 +50,7 @@ def run(image_path: str, output_csv: str) -> None:
 
     lower_red1, upper_red1 = np.array([0, 50, 50]), np.array([10, 255, 255])
     lower_red2, upper_red2 = np.array([170, 50, 50]), np.array([180, 255, 255])
+    lower_blue, upper_blue = np.array([110, 50, 100]), np.array([130, 255, 255])
 
     mask_red = cv2.inRange(hsv, lower_red1, upper_red1) + cv2.inRange(hsv, lower_red2, upper_red2)
     mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
@@ -46,6 +61,13 @@ def run(image_path: str, output_csv: str) -> None:
     all_data = right_ear_data + left_ear_data
 
     df = pd.DataFrame(all_data, columns=["Frequency (Hz)", "Threshold (dB HL)", "Ear"])
+    df = normalize(df, image)
+    df = drop_bone_data(df)
 
     print(df)
+    print(df.shape[0])
+    df.to_csv(output_csv, index=False)
 
+    # cv2.imshow("Detected Points", image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
