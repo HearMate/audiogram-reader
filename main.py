f676ebe2ddf418151f1d14e04ed8481685e04077
Stanisław Grochowski <grochowski0stanislaw@gmail.com>
2025-05-12 20:03:19 +0200

    fix #12 add anomaly detection and its status in response

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import json
-from audiogram_parser_1 import *
 from flask import Flask, request
 from flask_cors import CORS
+
+from audiogram_parser_1 import *
+from anomaly_detection import *
 
 app = Flask(__name__)
 CORS(app)
@@ -19,12 +21,12 @@ def upload_image():
 
     file = request.files['image']
     df = process_audiogram_image(file)
+    anomaly_status = detect_anomaly(df)
     result = df.groupby('Ear').apply(
         lambda g: dict(zip(g['Frequency (Hz)'], g['Threshold (dB HL)']))
     ).to_dict()
-    result.update({'Version': '1'})
+    result.update({'Status': anomaly_status, 'Version': '2'})
     json_str = json.dumps(result, indent=2)
-    print(json_str)
     return json_str
 
 
